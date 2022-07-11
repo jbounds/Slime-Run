1e7c5e0ea8652bfc78c7dc6ed173e497860b6265
Josh Bounds <Josh.Bounds@kclife.com>
2022-07-10 19:07:44 -0500

    Improve Slime Collision

--- Scripts/Slime.cs
+++ Scripts/Slime.cs
@@ -4,28 +4,46 @@ namespace Scripts
 {
     public class Slime : KinematicBody2D
     {
-        const float gravity = 150.0f;
+        // Vector2 Direction = new Vector2();
+        const float Gravity = 200.0f;
         public SlimeData SlimeData;
         public Vector2 Velocity;
 
         public override void _PhysicsProcess(float delta)
         {
-            Velocity.y += delta * gravity;
+            Velocity.y += delta * Gravity;
 
-            var collision2D = MoveAndCollide(Velocity * delta);
-            if (collision2D != null)
+            var collision2D = MoveAndSlide(Velocity, infiniteInertia: false);
+
+            // Want to slow down first collision with the basket, but continual collisions should be slowed down TODO FIX THIS.
+            if (GetSlideCount() > 0)
             {
-                Velocity = Velocity.Bounce(collision2D.Normal);
-                Velocity.y *= 0.2F;
-                Velocity.y *= 0.5F;
+                var collision = GetSlideCollision(0);
+                if (collision != null)
+                {
+                    Velocity = Velocity.Bounce(collision.Normal);
+                    if (collision.Collider is Slime)
+                    {
+                        Velocity.x *= 0.25F;
+                        if (Velocity.y < 0)
+                        {
+                            Velocity.y *= 0.25F;
+                        }
+                    }
+                    else if (collision.Collider is Player)
+                    {
+                        Velocity.x *= 0.75F;
+                        Velocity.y *= 0.75F;
+                    }
+                }
             }
         }
 
         public override void _Ready()
         {
             Velocity = new Vector2();
-            Velocity.y += 1.5F;
-            Velocity *= gravity;
+            Velocity.y += 1F;
+            Velocity *= Gravity;
         }
     }
 }
